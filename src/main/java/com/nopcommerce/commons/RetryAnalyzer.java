86bad0269b9f6008c6282ff1f254c54950e799b4
Rita <thanhngan060295@gmail.com>
2026-08-23 17:50:43 +0700

    Edit RetryAnalyzer

--- src/main/java/com/nopcommerce/commons/RetryAnalyzer.java
+++ src/main/java/com/nopcommerce/commons/RetryAnalyzer.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
     private int retryCount = 0;
-    private static final int MAX_RETRY = 2;
+    private static final int MAX_RETRY = 1;
 
     @Override
     public boolean retry(ITestResult result) {
